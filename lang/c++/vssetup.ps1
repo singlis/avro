5ca32e37e6a39134c1acc7d0e07c020d478a8e31
Scott Inglis <singlis@microsoft.com>
2017-07-25 11:26:26 -0700

    Updated the vssetup ps1 file

--- lang/c++/vssetup.ps1
+++ lang/c++/vssetup.ps1
@@ -1,12 +1,16 @@
+
+# Call nuget to restore the packages
+& .\nuget.exe restore .\packages.config -PackagesDirectory .\packages
+
+# Copy the boost libraries to a single directory so that they can be found by cmake
 $source=".\packages"
 $destination=".\packages\boost.1.64.0.0\lib\native\address-model-64"
+
 $lib_dir_exists = Test-Path $destination
 if ($lib_dir_exists) {
     Remove-Item $destination -Force -Recurse
-    New-Item -ItemType directory -Path=$destination
-} else {
-    New-Item -ItemType directory -Path=$destination
 }
+New-Item -ItemType directory -Path $destination
 
 $filter = [regex]"[^/]+\\address-model-64\\[^/]+$"
 $bin = Get-ChildItem -rec | Where-Object {!$_.PSIsContainer} | ForEach-Object -Process {$_.FullName}
@@ -14,3 +18,13 @@ $filtered = $bin -match $filter
 foreach ($item in $filtered) {
     Copy-Item -Path $item -Destination $destination
 }
+
+# Generate the VS projects
+$build_dir=".\build"
+if (Test-Path $build_dir) {
+    Remove-Item $build_dir -Force -Recurse
+}
+
+New-Item -ItemType directory -Path $build_dir
+& cd $build_dir;cmake .. -G "Visual Studio 14 2015 Win64"
+
